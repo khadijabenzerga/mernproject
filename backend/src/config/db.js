4516ed954ce0fb11c75271e818d568a47244c17b
khadijabenzerga <khadija.benzerga.1cpi.2017@gmail.com>
2023-06-24 12:21:24 +0100

    first commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const letsConnect = await mongoose.connect(process.env.MONGO_URI, {
+    const letsConnect = await mongoose.connect('mongodb+srv://khadijabenzerga1cpi2017:khadija2018@cluster0.7ustwsc.mongodb.net/', {
       useNewUrlParser: true,
-    });
+    }); 
     console.log(`MongoDB is connected`);
   } catch (err) {
     console.log(`MongoDB Error : ${err.message}`);
